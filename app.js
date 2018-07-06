38da7ed930ab70876815b1bc41e8f38540196f1e
Somtoo <you@example.com>
2018-07-06 14:56:37 +0100

    Fist Commit

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express=require("express"),
-    app=express()    
-    
+    app=express()   
+
+   
     app.get("/",function(req,res){
         res.render("landing"); 
      });
      
 app.set("view engine","ejs");
+app.use(express.static(__dirname+"/public"));
 app.listen(3000,function(){
     console.log("The Open Labs Has Started!");
 });
---
+++ public/scripts/script.js
@@ -0,0 +1,6 @@
+/* Preloader */
+$(window).on("load",function() {
+    $("#stats").fadeOut();
+    $("#preloader").delay(500).fadeOut();
+});
+
