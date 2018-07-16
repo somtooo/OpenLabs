2beaccdafd4728cce2a3b11ad8b967f871c7b37f
monoblack21 <kalming@yahoo.com>
2018-07-16 14:10:26 +0300

    removing javascript syntax sins and purifying somtoo's soul

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var express=require("express"),
-    app=express()   
+var express = require("express");
+var app = express();
 
-   
-    app.get("/",function(req,res){
-        res.render("landing"); 
-     });
-     
-app.set("view engine","ejs");
-app.use(express.static(__dirname+"/public"));
-app.listen(3000,function(){
+
+app.get("/", function(req, res) {
+    res.render("landing");
+});
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.listen(3000, function() {
     console.log("The Open Labs Has Started!");
 });
