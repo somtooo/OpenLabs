f5408b09f865eea8da46138528a5a638f5b74a39
Somtoo <you@example.com>
2018-07-24 14:35:59 +0100

    Add College page

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.get("/", function(req, res) {
     res.render("landing");
 });
 
+app.get("/college", function(req, res) {
+    res.render("college");
+});
+
+
+
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.listen(3000, function() {
